b678acdcb4a6de79777eedd41a085aebc0728259
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-16 12:30:57 +0500

    Connect to Wifi

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
+//% weight=6 color=#2699BF icon="\uf110" block="CW01_MQTT"
+namespace CW01_MQTT {
 
-//% weight=6 color=#2699BF icon="\uf110" block="CW01"
-namespace CW01 {
+    let res: string = ""
 
+    serial.redirect(SerialPin.P1, SerialPin.P0, 115200)
+
+//% blockId="connectToWifi" block="connect to WiFi SSID %SSID, Password %PSK"
+    export function connectToWifi(SSID: string, PSK: string): boolean {
+        serial.writeString("AT+CWMODE=1\r\n")
+        basic.pause(100)
+        serial.writeString("AT+CWJAP=\"" + SSID + "\",\"" + PSK + "\"\r\n")
+        basic.pause(5000)
+        res = serial.readString()
+        console.log(res)
+        return true
+    }
 } 
