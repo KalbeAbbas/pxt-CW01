eb1f40b6c90ea00b49a8dedd5d225d24a10d7537
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-16 12:37:37 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -5,7 +5,8 @@ namespace CW01_MQTT {
 
     serial.redirect(SerialPin.P1, SerialPin.P0, 115200)
 
-//% blockId="connectToWifi" block="connect to WiFi SSID %SSID, Password %PSK"
+    //% weight=91
+    //% blockId="connectToWifi" block="connect to WiFi SSID %SSID, Password %PSK"
     export function connectToWifi(SSID: string, PSK: string): boolean {
         serial.writeString("AT+CWMODE=1\r\n")
         basic.pause(100)
