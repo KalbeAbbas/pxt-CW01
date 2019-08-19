6be3fabf2a5ec3ec137968aa6e17ce3271edd088
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-19 16:18:41 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 
-namespace CW01_MQTT {
+namespace CW01_HTTP {
 
     let res: string = ""
 
     serial.redirect(SerialPin.P1, SerialPin.P0, 115200)
+
+    basic.pause(100)
+    serial.writeString("AT+TEST=0\r\n")
+    basic.pause(100)
+    serial.writeString("AT+TEST\r\n")
+    basic.pause(100)
+    serial.writeString("AT+TEST=1\r\n")
 
     //% weight=91
     //% blockId="connectToWifi" block="connect to WiFi SSID %SSID, Password %PSK"
@@ -12,7 +19,11 @@ namespace CW01_MQTT {
         basic.pause(100)
         serial.writeString("AT+CWJAP=\"" + SSID + "\",\"" + PSK + "\"\r\n")
         basic.pause(10000)
-        res = serial.readString()
-        serial.writeString(res)
+    }
+    //% weight=91
+    //% blockId="connectToWifi" block="connect to AllThingsTalk HTTP server"
+    export function connectToATT(): void {
+        serial.writeString("AT+CIPSTART=\"TCP\",\"api.allthingstalk.io\",80\r\n")
+        basic.pause(100)
     }
 } 
