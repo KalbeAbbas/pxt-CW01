78a276d8c55bd108f2e6f06d391ac24f57eb1ae4
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-19 17:50:47 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -39,8 +39,8 @@ namespace CW01_HTTP {
         asset_name = asset
         serial.writeString("AT+CIPMODE=0\r\n")
         basic.pause(100)
-        let payload: string = "{\"value\":" + value + "}"
-        let request: string = "PUT /device/" + DEVICE_ID + "/asset/" + asset_name + "/state HTTP/1.0\r\n" +
+        let payload: string = "{\"value\":" + "\""+ value+"\"" + "}"
+        let request: string = "PUT /device/" + DEVICE_ID + "/asset/" + asset_name + "/state HTTP/1.1\r\n" +
             "Authorization: Bearer " + TOKEN + "\r\n"+
         "Content-Type: application/json\r\n\r\n" + payload + "\r\n"
 
