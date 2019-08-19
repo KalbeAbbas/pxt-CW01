729155e2fa54a1152fa0f3991fac440759ba8177
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-19 17:06:03 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@
 namespace CW01_HTTP {
 
     let res: string = ""
+    let TOKEN: string = ""
+    let DEVICE_ID: string = ""
+    let asset_name: string = ""
 
     serial.redirect(SerialPin.P1, SerialPin.P0, 115200)
 
@@ -21,9 +24,29 @@ namespace CW01_HTTP {
         basic.pause(10000)
     }
     //% weight=91
-    //% blockId="connectToWifi" block="connect to AllThingsTalk HTTP server"
-    export function connectToATT(): void {
+    //% blockId="connectToATT" block="connect to ATT with TOKEN %TKN and DEVICE_ID %ID"
+    export function connectToATT(TKN: string, ID: string): void {
+        DEVICE_ID = ID
+        TOKEN = TKN
         serial.writeString("AT+CIPSTART=\"TCP\",\"api.allthingstalk.io\",80\r\n")
+
         basic.pause(100)
+    }
+
+    //% weight=91
+    //% blockId="connectToWifi" block="Send String %value to ATT Asset %asset_name"
+    export function IoTSendStringToATT(value: string, asset: string): void {
+        asset_name = asset
+        serial.writeString("AT+CIPMODE=0\r\n")
+        basic.pause(100)
+        let payload: string = "{\"value\":" + value + "}"
+        let request: string = "PUT /device/" + DEVICE_ID + "/asset/" + asset_name + "/state\r\n" +
+            "Authorization: Bearer " + TOKEN + "\r\n"+
+        "Content-Type: application/json\r\n\r\n" + payload + "\r\n"
+
+        serial.writeString("AT+CIPSEND=" +(request.length).toString()+ "\r\n")
+        basic.pause(100)
+        serial.writeString(request)
+        basic.pause(500)
     }
 } 
