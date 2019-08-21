30578863ccf18fc3f4aec3b44e8b80e18a809cba
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-21 20:27:14 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -200,7 +200,7 @@ namespace CW01_HTTP {
         let password_len: string = (pins.packBuffer("!H", [password.length])).toString()
         let msg_part_two = client_id_len + client_id + username_len + username + password_len + password
 
-        serial.writeString("AT+CIPSEND=" + "7" + NEWLINE)
+        serial.writeString("AT+CIPSEND=" + "9" + NEWLINE)
         basic.pause(1000)
         /*serial.writeBuffer(pins.packBuffer("!B", [4]))
         serial.writeBuffer(pins.packBuffer("!B", [4]))*/
@@ -209,7 +209,7 @@ namespace CW01_HTTP {
         serial.writeBuffer(pins.packBuffer("!B", [msg_part_one.length + connect_flags.length + keep_alive.length + msg_part_two.length]))
         serial.writeBuffer(pins.packBuffer("!H", [4]))
         serial.writeString("MQTT")
-        serial.writeBuffer(pins.packBuffer("!B",[4]))
+        serial.writeBuffer(pins.packBuffer("!B", [4]))
         /*serial.writeBuffer(connect_flags) // flags
         serial.writeBuffer(keep_alive) //keep alive
         serial.writeString(msg_part_two) //string data*/
