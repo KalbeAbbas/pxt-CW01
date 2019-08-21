e79ffb198e5f47489f7ef32c364440362759d8a0
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-08-21 20:59:50 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -200,7 +200,7 @@ namespace CW01_HTTP {
         let password_len: Buffer = pins.packBuffer("!H", [password.length])
         //let msg_part_two = client_id_len + client_id + username_len + username + password_len + password
 
-        serial.writeString("AT+CIPSEND=" + "81" + NEWLINE)
+        serial.writeString("AT+CIPSEND=" + "78" + NEWLINE)
         basic.pause(1000)
         /*serial.writeBuffer(pins.packBuffer("!B", [4]))
         serial.writeBuffer(pins.packBuffer("!B", [4]))*/
