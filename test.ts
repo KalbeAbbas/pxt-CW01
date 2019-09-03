088b7694fa66c1e0f3056dc1c3c995af06757404
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-03 18:21:28 +0500

    Updates.

--- test.ts
+++ test.ts
@@ -1,1 +1,10 @@
 // tests go here; this will not be compiled when this package is used as a library
+
+CW01_HTTP.begin()
+CW01_HTTP.connectToWifi("SSID", "PSK")
+CW01_HTTP.connectToATT("TOKEN", "ID")
+basic.forever(function () {
+    CW01_HTTP.IoTSendStateToATT(true, "ASSET_NAME")
+    basic.showString(CW01_HTTP.IoTgetATTAssetValue("ASSET_NAME"))
+    CW01_HTTP.IoTSendStateToATT(false, "ASSET_NAME")
+})
