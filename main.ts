dd398cf0e1ddba1c6c02a2c6ea494ded9cc3fdc4
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-04 14:39:45 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -198,14 +198,16 @@ namespace CW01_HTTP {
     //% weight=91 color=#f2ca00
     //% group="Ubidots"
     //% blockId="IoTgetValuefromUbidots" block="Get Value from Ubidots Device %device Variable %variable"
-    /*export function IoTgetValuefromUbidots(device: string, variable: string): void {
+    export function IoTgetValuefromUbidots(device: string, variable: string): string {
+        res = ""
+        let value:string
         let request: string = "GET /api/v1.6/devices/" + device + "/" + variable + "/values HTTP/1.1" + NEWLINE +
             "Host: things.ubidots.com" + NEWLINE +
             "User-Agent: CW01/1.0" + NEWLINE +
+            "Accept: " + NEWLINE +
             "X-Auth-Token: " + TOKEN + NEWLINE +
-            "Content-Type: application/json" + NEWLINE +
-            //"Accept: " + NEWLINE +
-            "Content-Length: " + (payload.length).toString() + NEWLINE + NEWLINE + payload + NEWLINE
+            "Content-Type: application/json" + NEWLINE + NEWLINE
+            //"Content-Length: " + (payload.length).toString() + NEWLINE + NEWLINE + payload + NEWLINE
 
 
 
@@ -214,10 +216,15 @@ namespace CW01_HTTP {
         serial.writeString(request)
         basic.pause(10)
         serial.readString()
-        basic.pause(1000)
+        basic.pause(400)
+        serial.writeString("AT+CIPRECVDATA=200" + NEWLINE)
+        basic.pause(100)
+        res += serial.readString()
+        value = res
 
-        get_status()
-}*/
+        return value
+
+    }
 
     //% weight=91 color=#f2ca00
     //% group="Ubidots"
