d80c2665ffba6abe648673f30e986727806780ae
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-16 16:39:05 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ enum user {
 
 }
 
-//% groups=["Common",ATT", "Ubidots", "others"]
+//% groups=["Common",ATT", "Ubidots", "Azure", "others"]
 //% weight=6 color=#2699BF icon="\uf110" block="CW01 HTTP"
 namespace CW01_HTTP {
 
