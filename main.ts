dfe5231bb5efa023adeebedcd838041d0c55a922
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-16 17:41:16 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -294,8 +294,8 @@ namespace cw01HTTP {
 
     //% weight=91 color=#4B0082
     //% group="Azure"
-    //% blockId="connectToAzure" block="connect to Azure %user| with TOKEN %TKN"
-    export function connectToAzure(User: user, TKN: string): void {
+    //% blockId="connectToAzure" block="connect to Azure with access enpoint #access"
+    export function connectToAzure(access: string): void {
     }
 
     //% weight=91 color=#f2ca00
