73971d5775a921f545d56a30c2299b61bb7d2c19
KalbeAbbas <kalbeabbas142@gmail.com>
2019-09-30 19:53:48 +0500

    Updates.

--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
 // tests go here; this will not be compiled when this package is used as a library
 
-cw01HTTP.connectToWifi("SSID", "PSK")
-cw01HTTP.connectToATT("DEVICE_TOKEN", "DEVICE_ID")
+cw01.connectToWifi("SSID", "PSK")
+cw01.connectToATT("DEVICE_TOKEN", "DEVICE_ID")
 basic.forever(function () {
-    cw01HTTP.IoTSendStateToATT(true, "boolTest")
-    basic.showString(cw01HTTP.IoTgetATTAssetValue("6"))
-    cw01HTTP.IoTSendStateToATT(false, "boolTest")
+    cw01.IoTSendStateToATT(true, "boolTest")
+    basic.showString(cw01.IoTgetATTAssetValue("6"))
+    cw01.IoTSendStateToATT(false, "boolTest")
 })
