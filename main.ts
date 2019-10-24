a471d5d78817cf7d11fc9b59c8359180ac363887
KalbeAbbas <kalbeabbas142@gmail.com>
2019-10-24 12:57:29 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -508,7 +508,7 @@ namespace cw01 {
         serial.writeString("AT+CIPRECVDATA=200" + NEWLINE)
         basic.pause(300)
 
-        serial.onDataReceived(",", function () {
+        serial.onDataReceived("+", function () {
             if((serial.readString()).includes("IPD"))
             {
                 cw01.IoTMQTTGetData()
