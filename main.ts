688bae7f1640daee6b914ca756258d1d82802293
KalbeAbbas <kalbeabbas142@gmail.com>
2019-12-12 14:39:20 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -811,6 +811,7 @@ namespace cw01 {
 
                 if ((serial.readString()).includes("IPD")) {
                     IoTMQTTGetData()
+                    basic.showString(cw01_mqtt_vars.enable_event.toString())
                     if (cw01_mqtt_vars.enable_event)
                         handler()
                 }
