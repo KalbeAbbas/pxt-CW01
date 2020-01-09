375e0beddba67d60e440c60b7d0e0e887a6227ed
KalbeAbbas <kalbeabbas142@gmail.com>
2020-01-09 21:08:43 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -810,6 +810,7 @@ namespace cw01 {
                         //if (cw01_mqtt_vars.enable_event_1 || cw01_mqtt_vars.enable_event_2)
                         handler()
                     } else if (ctrl_pkt == 160) {
+                        ctrl_pkt = 0
                         serial.writeString("AT+CIPRECVDATA=200" + cw01_vars.NEWLINE)
                         basic.pause(100)
                     }
