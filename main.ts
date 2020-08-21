7297269680360c72f2746b15b3a9cf54ec5ffc74
KalbeAbbas <kalbeabbas142@gmail.com>
2020-08-21 19:20:12 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -538,7 +538,15 @@ namespace cw01 {
 
                     serial.writeString("AT+CIPRECVDATA=1,1" + cw01_vars.NEWLINE)
                     basic.pause(100)
-                    serial.readBuffer(20)
+                    let buf = serial.readBuffer(50)
+                    for(let i = 0; i < 50; i++)
+                    {
+                        if(buf[i] == 58)
+                        {
+                            basic.showString("Hello!")
+                            break
+                        }
+                    }
                     ctrl_pkt = (pins.unpackBuffer("!B", serial.readBuffer(1)))[0]
                     basic.showNumber(ctrl_pkt)
 
