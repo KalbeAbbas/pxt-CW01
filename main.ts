a809d7b72d719c795351f4420db4b69b171c972a
KalbeAbbas <kalbeabbas142@gmail.com>
2020-09-04 15:43:01 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -513,6 +513,7 @@ namespace cw01 {
     */
     //% weight=91
     //% group="ATT"
+    //% block= "CW01 on command received"
     export function onCommandReceived(handler: (value: string, asset_name: string) => void) {
 
         control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
