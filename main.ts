084b464dd520dcb68a01423b05710c9c801e731b
KalbeAbbas <kalbeabbas142@gmail.com>
2020-09-04 15:56:18 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -514,7 +514,7 @@ namespace cw01 {
     //% group="ATT"
     //% block="CW01 on command received"
     //% blockId =onCommandReceived
-    export function onCommandReceived(handler: () => void) {
+    export function onCommandReceived(handler: (value: string, asset_name: string) => void) {
 
         control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
 
@@ -566,7 +566,7 @@ namespace cw01 {
 
                     if (ctrl_pkt == 48) {
                         IoTMQTTGetData()
-                        handler()
+                        handler(IoTATTGetValue(), IoTATTGetAssetName())
                     } else if (ctrl_pkt == 208) {
                         ctrl_pkt = 0
                         serial.writeString("AT+CIPRECVDATA=1,200" + cw01_vars.NEWLINE)
